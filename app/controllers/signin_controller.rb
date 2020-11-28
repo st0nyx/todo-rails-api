a5dc7ef27c705f5bdc670771f826cf06128d9eff
Bruno Steinmann <bruno.steinmann@gmx.com>
2020-11-28 16:30:24 +0100

    specs for signin

--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -1,9 +1,12 @@
 class SigninController < ApplicationController
+
   def create
     user = User.find_by!(email: params[:email])
     if user.authenticate(params[:password])
       payload = { user_id: user.id }
-      session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
+      session = JWTSessions::Session.new(
+          payload: payload,
+          refresh_by_access_allowed: true)
       tokens = session.login
       response.set_cookie(JWTSessions.access_cookie,
                           value: tokens[:access],
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post 'signin', controller: :signin, action: :create
   post 'signup', controller: :signup, action: :create
 
 end
