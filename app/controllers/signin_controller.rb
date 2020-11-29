47389766bf030ca68cb76032cef6cd20c9d2b3c6
Bruno Steinmann <bruno.steinmann@gmx.com>
2020-11-29 09:20:02 +0100

    added logout to signin controller

--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -1,4 +1,5 @@
 class SigninController < ApplicationController
+  before_action :authorize_access_request!, only: [:destroy]
 
   def create
     user = User.find_by!(email: params[:email])
@@ -18,4 +19,17 @@ class SigninController < ApplicationController
       not_authorized
     end
   end
+
+  def destroy
+    session = JWTSessions::Session.new(payload: payload)
+    session.flush_by_access_payload
+    render json: :ok
+  end
+
+  private
+
+  def not_found
+    render json: { error: 'Cannont find such email/password combination' }, status: :not_found
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   post 'signin', controller: :signin, action: :create
   post 'signup', controller: :signup, action: :create
   post 'refresh', controller: :refresh, action: :create
+  delete 'signin', controller: :signin, action: :destroy
 
   resources :todos
 end
