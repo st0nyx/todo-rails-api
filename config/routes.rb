35b03fc493b1a907c84aa319dd66e695d3d58463
Bruno Steinmann <bruno.steinmann@gmx.com>
2020-11-29 08:27:07 +0100

    specs for refresh

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   post 'signin', controller: :signin, action: :create
   post 'signup', controller: :signup, action: :create
-
+  post 'refresh', controller: :refresh, action: :create
 end
