967f4b00f1468dfcde45cc972a4eaa736a189b7b
Bruno Steinmann <bruno.steinmann@gmx.com>
2020-11-29 09:15:38 +0100

    specs for todos

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   post 'signin', controller: :signin, action: :create
   post 'signup', controller: :signup, action: :create
   post 'refresh', controller: :refresh, action: :create
+
+  resources :todos
 end
